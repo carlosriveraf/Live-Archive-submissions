8e81a70e1ee77ec9a56b0e8906eefd481cd1c173
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:15:58 -0500

    Update and rename 2884 - The Splitting Club to 2884 - The Splitting Club.cpp

--- 2884 - The Splitting Club.cpp
+++ 2884 - The Splitting Club.cpp
@@ -6,30 +6,30 @@ int main () {
 	
 	while( cin>>K>>R ) {
 		
-		if( K==0 && R==0.0 ) { return 0; }
+		if( K == 0 && R == 0.0 ) { return 0; }
 		
 		vector<int> members(121);
-		while(K--){
+		while( K-- ) {
 			int n, m;
 			cin>>n>>m;
-			members[m]+=n;
+			members[m] += n;
 		}
 		
 		vector<int> section;
-		for( int i=1 ; i<121 ; i++ ) {
-			if( members[i]!=0 ) {
+		for( int i = 1 ; i < 121 ; i++ ) {
+			if( members[i] != 0 ) {
 				section.push_back( members[i] );
 			}
 		}
 		
-		sort( section.begin() , section.end() );
+		sort( section.begin(), section.end() );
 		
-		int a=0, b=0;
-		int count=0;
+		int a = 0, b = 0;
+		int count = 0;
 		while( b < int( section.size() ) ) {
 			
-			if( double(section[b]) / double(section[a]) <=R ) { b++; }
-			else{ a=b; count++; }
+			if( double(section[b]) / double(section[a]) <= R ) { b++; }
+			else{ a = b; count++; }
 			
 		}
 		
